f087b6e89ec532520f5eca3347fa6dc09e4a1dbb
Seyi <syglemelg@gmail.com>
2025-09-02 22:29:16 +0100

    Removed the parameters in the constructors. Then add a string literal value to the description and status variables.

--- task_tracker.py
+++ task_tracker.py
@@ -2,10 +2,9 @@ import argparse, json
 from datetime import date
 
 class Task:
-    def __init__(self, id, description, status):
-        self.id = id
-        self.description = description
-        self.status = status
+    def __init__(self):
+        self.description = "task"
+        self.status = "TODO"
         self.created_at = date.today()
         self.updated_at = date.today()
 
@@ -13,11 +12,34 @@ class Task:
 
     def add(self, task_description):
         with open("tasks.json", "a") as file:
-            tasks_list = json.load(file)
-        tasks_list.append(task_description)
+            self.tasks_list = json.load(file)
+        self.tasks_list.append(task_description)
         print("Task added!")
 
 
+def main():
+    parser = argparse.ArgumentParser(
+        description="A simple Task CLI Tracker"
+    )
+
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Subcommand: add
+    greet_parser = subparsers.add_parser("add", help="Add a task description")
+    greet_parser.add_argument("description", type=str, help="task description")
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Dispatch commands
+    if args.command == "add":
+        Task.add(args.name)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
 
     
 
