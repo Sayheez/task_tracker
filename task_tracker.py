cfe26ea5aa719fe906c30394806a11dca22094d0
Seyi <syglemelg@gmail.com>
2025-09-02 22:35:55 +0100

    Using argparse module to initialize the parser.

--- task_tracker.py
+++ task_tracker.py
@@ -7,8 +7,16 @@ class Task:
         self.status = "TODO"
         self.created_at = date.today()
         self.updated_at = date.today()
+        self.tasks_list = []
 
-    # tasks = []  # A list where the 
+                # Initialize parser
+        self.parser = argparse.ArgumentParser(
+            description="Class-based TASK-CLI TRACKER"
+        )
+        self.subparsers = self.parser.add_subparsers(dest="command", help="Available commands")
+
+        # Register commands
+        self._register_commands()
 
     def add(self, task_description):
         with open("tasks.json", "a") as file:
